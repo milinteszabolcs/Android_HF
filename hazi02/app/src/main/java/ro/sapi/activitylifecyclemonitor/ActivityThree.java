7ec0da342093084d73bcae7d2f755d58da5ec555
milinteszabolcs <72197941+milinteszabolcs@users.noreply.github.com>
2020-10-20 11:26:11 +0300

    Hazi02

---
+++ hazi02/app/src/main/java/ro/sapi/activitylifecyclemonitor/ActivityThree.java
@@ -0,0 +1,53 @@
+package ro.sapi.activitylifecyclemonitor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three);
+        Log.d("Statusz","ActivityThree: onCreate()");
+    }
+
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Statusz","ActivityThree: onStart()");
+    }
+
+    /** Called when the activity has become visible. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Statusz","ActivityThree: onResume()");
+    }
+
+    /** Called when another activity is taking focus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Statusz","ActivityThree: onPause()");
+    }
+
+    /** Called when the activity is no longer visible. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Statusz","ActivityThree: onStop()");
+    }
+
+    /** Called just before the activity is destroyed. */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("Statusz","ActivityThree: onDestroy()");
+    }
+}
